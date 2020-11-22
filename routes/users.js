36b762dd51260ef1eb831ac6de5b22f0de08378a
onifademola <onifademola@gmail.com>
2020-11-22 16:32:04 +0100

    final commit

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const User = require('../models/User');
+const User = require("../models/User");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const users = await User.find();
     res.json(users);
   } catch (err) {
     console.log(err.message);
-    res.status(500).send('Sevrer Error');
-  }  
+    res.status(500).send("Sevrer Error");
+  }
+});
+
+router.post("/search", async (req, res) => {
+  const [name] = req.body.searchTerm;
+  // const query = {
+  //   $or: [
+  //     { firstName: { $regex: /${req.body.searchTerm}/i } },
+  //     { lastName: { $regex: /${req.body.searchTerm}/i } },
+  //   ],
+  // };
+  const query = {
+    $or: [
+      { firstName: req.body.searchTerm },
+      { lastName: req.body.searchTerm },
+    ],
+  };
+  console.log(query)
+  try {
+    const users = await User.find(query);
+    res.json(users);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Sevrer Error");
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const http = require("http");
 
 const app = express();
 connectDB();
 
+// let miniLogger = (req, res, next) => {
+//   let current_datetime = new Date();
+//   let formatted_date =
+//     current_datetime.getFullYear() +
+//     "-" +
+//     (current_datetime.getMonth() + 1) +
+//     "-" +
+//     current_datetime.getDate() +
+//     " " +
+//     current_datetime.getHours() +
+//     ":" +
+//     current_datetime.getMinutes() +
+//     ":" +
+//     current_datetime.getSeconds();
+//   let method = req.method;
+//   let url = req.url;
+//   let status = res.statusCode;
+//   let body = req.body;
+//   let log = `[${formatted_date}] ${method}:${url} ${status} ReqBody:${body}`;
+//   console.log(log);
+//   next();
+// };
+
 app.get('/', (req, res) => res.json({message: 'API is live!'}));
+// app.use(miniLogger);
 
 // enable CORS without external module
 app.use(function (req, res, next) {
@@ -12,6 +37,8 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(express.json({ extended: false }));
 
 // API Routes
 app.use('/api/users', require('./routes/users'));
